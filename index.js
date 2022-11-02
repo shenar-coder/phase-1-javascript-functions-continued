e053cfa4db15b646f57ee59569cf4fd665685011
shinar3@gmail.com <110408797+shenar-coder@users.noreply.github.com>
2022-11-02 09:25:17 -0400

    commit tree f8ae6771b95b2ab82d1228622a1ec0b58f83897c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+/*function razzle() {
+console.log( "you've been razzled!");   
+
+
+}razzle();
+*/
+
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (location = 'go to the office') {
+    return `This Monday, I will ${location}.`
+
+
+}
+
+     function wrapAdjective (visualFlair = '*') {
+        return function inner (anyWord = 'special'){
+           return `You are ${visualFlair}${anyWord}${visualFlair}!`
+      
+    
+    }
+     
+
+
+
+}
+/*
+function wrapAdjective (visualFlair = '*') {
+    return function inner (anyWord = 'special') {
+        return `You are ${visualFlair}${anyWord}${visualFlair}!`
+    }
+}
+*/
